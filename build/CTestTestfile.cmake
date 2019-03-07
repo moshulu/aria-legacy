66fe2da492acd03f6c91062f8fff11ec45d1b286
root <root@matt-pallas>
2019-03-07 17:58:22 -0500

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/local/Aria/src
+# Build directory: /usr/local/Aria/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
